dbc20d319efce22364bf56b0c1552ae0364902a3
moriMaroo17 <morimaroo17@gmail.com>
2022-06-19 17:14:07 +0300

    task 1 fixed again

--- task1/main.go
+++ task1/main.go
@@ -3,18 +3,23 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/beevik/ntp"
 )
 
-func printTime() {
+func printTime() error {
 	time, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	fmt.Printf("%s\n", time)
+	return nil
 }
 
 func main() {
-	printTime()
+	if err := printTime(); err != nil {
+		l := log.New(os.Stderr, "", 1)
+		l.Fatal(err)
+	}
 }
