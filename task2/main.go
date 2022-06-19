2053c3f72b73455396cafde66914e05718255f3b
moriMaroo17 <morimaroo17@gmail.com>
2022-06-19 18:18:32 +0300

    task 2 completed

---
+++ task2/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func parseString(stringForUnpack string) (string, error) {
+	if len(stringForUnpack) == 0 {
+		return "", nil
+	}
+	sb := new(strings.Builder)
+	runeArray := []rune(strings.ToLower(stringForUnpack))
+	if (runeArray[0] < 97 && runeArray[0] != 92) || runeArray[0] > 122 {
+		return "", errors.New("wrong first symbol")
+	}
+	for i := 0; i <= len(runeArray)-1; i++ {
+		if runeArray[i] == 92 {
+			sb.WriteRune(runeArray[i+1])
+			i++
+		} else if runeArray[i] >= 97 && runeArray[i] <= 122 {
+			sb.WriteRune(runeArray[i])
+		} else if runeArray[i] >= 48 && runeArray[i] <= 57 {
+			num := new(strings.Builder)
+			for _, symb := range runeArray[i:] {
+				if symb >= 48 && symb <= 57 {
+					num.WriteRune(symb)
+				} else {
+					break
+				}
+			}
+			numAsInt, err := strconv.Atoi(num.String())
+			if err != nil {
+				return "", err
+			}
+			sb.WriteString(strings.Repeat(string(runeArray[i-1]), numAsInt-1))
+		}
+	}
+	return sb.String(), nil
+}
+
+func main() {
+	result, err := parseString("a4bc2d5e")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(result)
+	result, err = parseString("abcd")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(result)
+	// result, err = parseString("45")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println(result)
+	result, err = parseString("")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(result)
+	result, err = parseString(`qwe\4\5`)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(result)
+	result, err = parseString(`qwe\45`)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(result)
+	result, err = parseString(`qwe\\5`)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(result)
+}
