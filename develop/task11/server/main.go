7f22a208dc671df9b6e20c455688983f1aa9feb1
moriMaroo17 <morimaroo17@gmail.com>
2022-07-07 21:02:41 +0300

    added handlers for get requests

--- develop/task11/server/main.go
+++ develop/task11/server/main.go
@@ -51,7 +51,7 @@ func deleteHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	response, _ := json.Marshal(fmt.Sprintf(`{"result": "event with id %d was successfully deleted"}`, eventIDasInt))
-	w.WriteHeader(200)
+	// w.WriteHeader(200)
 	w.Write(response)
 
 }
@@ -84,7 +84,7 @@ func createHandler(w http.ResponseWriter, r *http.Request) {
 
 	id := storage.Create(e)
 	response, _ := json.Marshal(fmt.Sprintf(`{"result": "event with id %d was successfully created"}`, id))
-	w.WriteHeader(200)
+	// w.WriteHeader(200)
 	w.Write(response)
 }
 
@@ -137,18 +137,114 @@ func updateHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	response, _ := json.Marshal(fmt.Sprintf(`{"result": "event with id %d was successfully updated"}`, eventIDasInt))
-	w.WriteHeader(200)
+	// w.WriteHeader(200)
+	w.Write(response)
+}
+
+func getByDayHandler(w http.ResponseWriter, r *http.Request) {
+	params := r.URL.Query()
+
+	userID := params.Get("user_id")
+	if userID == "" {
+		response, _ := json.Marshal(`{"error": "specify user id"}`)
+		w.WriteHeader(400)
+		w.Write(response)
+		return
+	}
+
+	date, err := time.Parse("2006-01-02", params.Get("date"))
+	if err != nil {
+		response, _ := json.Marshal(`{"error": "wrong date"}`)
+		w.WriteHeader(400)
+		w.Write(response)
+		return
+	}
+
+	result := storage.GetEventsByDay(userID, date)
+
+	response, err := json.Marshal(fmt.Sprintf(`{"result": "%v"}`, result))
+	if err != nil {
+		response, _ := json.Marshal(`{"error": "internal server error"}`)
+		w.WriteHeader(500)
+		w.Write(response)
+		return
+	}
+	w.Write(response)
+}
+
+func getByWeekHandler(w http.ResponseWriter, r *http.Request) {
+	params := r.URL.Query()
+
+	userID := params.Get("user_id")
+	if userID == "" {
+		response, _ := json.Marshal(`{"error": "specify user id"}`)
+		w.WriteHeader(400)
+		w.Write(response)
+		return
+	}
+
+	date, err := time.Parse("2006-01-02", params.Get("date"))
+	if err != nil {
+		response, _ := json.Marshal(`{"error": "wrong date"}`)
+		w.WriteHeader(400)
+		w.Write(response)
+		return
+	}
+
+	result := storage.GetEventsByWeek(userID, date)
+	response, err := json.Marshal(fmt.Sprintf(`{"result": "%v"}`, result))
+	if err != nil {
+		response, _ := json.Marshal(`{"error": "internal server error"}`)
+		w.WriteHeader(500)
+		w.Write(response)
+		return
+	}
+	w.Write(response)
+}
+
+func getByMonthHandler(w http.ResponseWriter, r *http.Request) {
+	params := r.URL.Query()
+
+	userID := params.Get("user_id")
+	if userID == "" {
+		response, _ := json.Marshal(`{"error": "specify user id"}`)
+		w.WriteHeader(400)
+		w.Write(response)
+		return
+	}
+
+	date, err := time.Parse("2006-01-02", params.Get("date"))
+	if err != nil {
+		response, _ := json.Marshal(`{"error": "wrong date"}`)
+		w.WriteHeader(400)
+		w.Write(response)
+		return
+	}
+
+	result := storage.GetEventsByMonth(userID, date)
+	response, err := json.Marshal(fmt.Sprintf(`{"result": "%v"}`, result))
+	if err != nil {
+		response, _ := json.Marshal(`{"error": "internal server error"}`)
+		w.WriteHeader(500)
+		w.Write(response)
+		return
+	}
 	w.Write(response)
 }
 
 func main() {
 	storage = calendar.NewStorage("storage.json")
+	storage.Restore()
 
 	r := http.NewServeMux()
 
 	r.HandleFunc("/create_event", createHandler)
 	r.HandleFunc("/update_event", updateHandler)
 	r.HandleFunc("/delete_event", deleteHandler)
+
+	r.HandleFunc("/events_for_day", getByDayHandler)
+	r.HandleFunc("/events_for_week", getByWeekHandler)
+	r.HandleFunc("/events_for_month", getByMonthHandler)
 
 	m := LoggingHandler(r)
 	http.ListenAndServe(":8080", m)
